d9b8dd74306495b20c7c48951303f000be5811be
rSSrini <112602626+rSSrini@users.noreply.github.com>
2023-03-31 12:18:24 -0500

    starting to build boards

--- Board.java
+++ Board.java
@@ -1,15 +1,29 @@
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 public class Board {
 
     private int location;
     private int boardType;
-    private ArrayList<HexTile> corners;
-    private HashSet<HexTile> tiles;
-
+    private HashMap<int[], HexTile> tiles;
+    String[] board1 = {"d", "d", "c", "w", "w", "f", "f", "f", "g", "g", "c", "castle"};
     public Board(int loc, int bT) {
         location = loc;
         boardType = bT;
     }
+
+    public void score() {
+
+    }
+    public int scoreFishermen() {}
+    public int scoreMiners() {}
+    public int scoreMerchants() {}
+    public int scoreWorkers() {}
+    public int scoreDiscoverers() {}
+    public int scoreKnights() {}
+    public int scoreHermits() {}
+    public int scoreLords() {}
+    public int scoreCitizens() {}
+    public int scoreFarmers() {}
+    public ArrayList<HexTile> findValidPlacements() {}
 
 }
