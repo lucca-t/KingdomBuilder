cd7145314afc1a01545fa561cef103d9dc67eea3
OmarRamadanCS <OmarRamadan@live.com>
2023-03-31 12:13:43 -0500

    git commit -Action.java

--- Action.java
+++ Action.java
@@ -1,2 +1,30 @@
 public class Action {
+    private int location;
+    private String type;
+    private boolean used;
+
+    public Action(int loc, String ty, boolean u){
+        location = loc;
+        type = ty;
+        used = u;
+
+    }
+    public String getType(){
+        return type;
+    }
+    public void setType(String ty){
+        type = ty;
+    }
+    public int getLoc(){
+        return location;
+    }
+    public void setLoc(int loc){
+        location = loc;
+    }
+    public void setUsed(boolean u){
+        used = u;
+    }
+    public boolean getUsed(){
+        return used;
+    }
 }
