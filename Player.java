84b1faf00cb84b0343af1eb5f3651da41ad35ab7
OmarRamadanCS <OmarRamadan@live.com>
2023-03-31 12:17:52 -0500

    git commit -Player.java

--- Player.java
+++ Player.java
@@ -1,2 +1,9 @@
-public class Player {
+import java.util.ArrayList;
+
+public class Player{
+    private int points;
+    private String settlementColor;
+    private int settlements;
+    private ArrayList<Action> availableActions;
+    private String currTerrain;
 }
