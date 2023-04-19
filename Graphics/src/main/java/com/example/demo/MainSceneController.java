2174ae5bb3c9acefef936a08b4fc0cb61e38579e
lucca-t <ninja.spinji@gmail.com>
2023-04-19 12:18:50 -0500

    git commit -Changes "added fx files and stuff"

---
+++ Graphics/src/main/java/com/example/demo/MainSceneController.java
@@ -0,0 +1,109 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.Label;
+        import javafx.scene.image.ImageView;
+
+public class MainSceneController {
+
+    @FXML
+    private ImageView bonusTile0;
+
+    @FXML
+    private ImageView bonusTile1;
+
+    @FXML
+    private ImageView bonusTile2;
+
+    @FXML
+    private ImageView bonusTile3;
+
+    @FXML
+    private ImageView bonusTile4;
+
+    @FXML
+    private ImageView bonusTile5;
+
+    @FXML
+    private ImageView bonusTile6;
+
+    @FXML
+    private ImageView bonusTile7;
+
+    @FXML
+    private Button confirmPlaceButton;
+
+    @FXML
+    private Label current;
+
+    @FXML
+    private ImageView currentTerrainCard;
+
+    @FXML
+    private Button finishTurnButton;
+
+    @FXML
+    private ImageView firstPlayerToken;
+
+    @FXML
+    private ImageView infoTile0;
+
+    @FXML
+    private ImageView infoTile1;
+
+    @FXML
+    private ImageView infoTile2;
+
+    @FXML
+    private ImageView infoTile3;
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private Label playerName;
+
+    @FXML
+    private ImageView pointCard0;
+
+    @FXML
+    private ImageView pointCard1;
+
+    @FXML
+    private ImageView pointCard2;
+
+    @FXML
+    private Button prevButton;
+
+    @FXML
+    private Button useBonusButton;
+
+    @FXML
+    void confirmPlace(ActionEvent event) {
+
+    }
+
+    @FXML
+    void finishTurn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void next(ActionEvent event) {
+
+    }
+
+    @FXML
+    void previous(ActionEvent event) {
+
+    }
+
+    @FXML
+    void useBonus(ActionEvent event) {
+
+    }
+
+}
+
