bee44bfc759b1000839c73872d7786385601c91f
Derrekyu <derrekyu@gmail.com>
2023-04-26 11:40:53 -0500

    enabled program to run?

--- Graphics/src/main/java/com/example/demo/HelloApplication.java
+++ Graphics/src/main/java/com/example/demo/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         //new KingdomBuilderMain();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainScene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("start-screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1424, 801);
         stage.setTitle("KingdomBuilder");
         stage.setScene(scene);
